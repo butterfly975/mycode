6bef5e84baadc5c3a2bd653a40ae145bc96a0f6c
Heather Thomas <heather.t975@gmail.com>
2018-12-12 10:29:34 -0500

    your commit message

---
+++ if-test/if-hostname.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+prompt_hostname_msg = ('please enter a hostname :')
+host_name = input(prompt_hostname_msg)
+host_name = host_name.upper()
+if host_name == 'MTG':
+    print('The hostname was found to be MTG')
+    print (host_name)
